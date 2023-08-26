accac515e344eae11d33552af53a27d45a134d63
kgw08003 <109195054+kgw08003@users.noreply.github.com>
2023-08-26 20:20:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,17 @@ from langchain.chains import RetrievalQA
 import streamlit as st
 import tempfile
 import os
+from streamlit_extras.buy_me_a_coffee import button
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+button(username="jocoding", floating=True, width=221)
 
 #제목
 st.title("ChatPDF")
 st.write("---")
+
+#OpenAI KEY 입력 받기
+openai_key = st.text_input('OPEN_AI_API_KEY', type="password")
 
 #파일 업로드
 uploaded_file = st.file_uploader("PDF 파일을 올려주세요!",type=['pdf'])
@@ -44,10 +51,20 @@ if uploaded_file is not None:
     texts = text_splitter.split_documents(pages)
 
     #Embedding
-    embeddings_model = OpenAIEmbeddings()
+    embeddings_model = OpenAIEmbeddings(openai_api_key=openai_key)
 
     # load it into Chroma
     db = Chroma.from_documents(texts, embeddings_model)
+
+    #Stream 받아 줄 Hander 만들기
+    from langchain.callbacks.base import BaseCallbackHandler
+    class StreamHandler(BaseCallbackHandler):
+        def __init__(self, container, initial_text=""):
+            self.container = container
+            self.text=initial_text
+        def on_llm_new_token(self, token: str, **kwargs) -> None:
+            self.text+=token
+            self.container.markdown(self.text)
 
     #Question
     st.header("PDF에게 질문해보세요!!")
@@ -55,10 +72,12 @@ if uploaded_file is not None:
 
     if st.button('질문하기'):
         with st.spinner('Wait for it...'):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+            chat_box = st.empty()
+            stream_hander = StreamHandler(chat_box)
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_key, streaming=True, callbacks=[stream_hander])
             qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
-            result = qa_chain({"query": question})
-            st.write(result["result"])
+            qa_chain({"query": question})
 
 
-# streamlit run main.py
+# streamlit run main.py 
+# openapi key를 카드 등록후 받아서 사용해야함
